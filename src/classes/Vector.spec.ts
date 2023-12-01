2f49b5309c81cac8d60aea3692fb7bdbc9d3002d
danyalready <doniyor.otamurodov@gmail.com>
2023-12-01 18:04:20 +0500

    test: Vector - unit

--- src/classes/Vector.spec.ts
+++ src/classes/Vector.spec.ts
@@ -1,5 +1,17 @@
-// import Vector from './Vector';
+import Vector from './Vector';
 
 describe('Vector class:', () => {
-    test('run', () => {});
+    test('unit value', () => {
+        const vectors = [
+            new Vector({ x: 4, y: 0 }),
+            new Vector({ x: 0, y: 11 }),
+            new Vector({ x: 7, y: 0 }),
+            new Vector({ x: 1, y: 1 }),
+            new Vector({ x: 3, y: 3 }),
+        ];
+
+        for (const vector of vectors) {
+            expect(vector.unit.magnitude).toBeCloseTo(1);
+        }
+    });
 });
