7d3c4f3e48a489347b50573fa878a17a987a7190
danyalready <doniyor.otamurodov@gmail.com>
2023-12-02 00:31:09 +0500

    test: Vector - normal

--- src/classes/Vector.spec.ts
+++ src/classes/Vector.spec.ts
@@ -1,17 +1,25 @@
 import Vector from './Vector';
 
+const vectors = [
+    new Vector({ x: 4, y: 0 }),
+    new Vector({ x: 0, y: 11 }),
+    new Vector({ x: 7, y: 0 }),
+    new Vector({ x: 1, y: 1 }),
+    new Vector({ x: 3, y: 3 }),
+    new Vector({ x: -7, y: -3 }),
+    new Vector({ x: -13, y: 70 }),
+];
+
 describe('Vector class:', () => {
     test('unit value', () => {
-        const vectors = [
-            new Vector({ x: 4, y: 0 }),
-            new Vector({ x: 0, y: 11 }),
-            new Vector({ x: 7, y: 0 }),
-            new Vector({ x: 1, y: 1 }),
-            new Vector({ x: 3, y: 3 }),
-        ];
-
         for (const vector of vectors) {
             expect(vector.unit.magnitude).toBeCloseTo(1);
+        }
+    });
+
+    test('normal value', () => {
+        for (const vector of vectors) {
+            expect(vector.normal).toMatchObject({ x: -vector.y, y: vector.x });
         }
     });
 });
