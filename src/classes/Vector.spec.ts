bce4a149b5666e02e4de33dc3a28f4ae31b77f06
danyalready <doniyor.otamurodov@gmail.com>
2023-12-02 10:49:40 +0500

    test: cover remaining Vector methods

--- src/classes/Vector.spec.ts
+++ src/classes/Vector.spec.ts
@@ -1,31 +1,54 @@
 import Vector from './Vector';
 
-const vectors = [
-    new Vector({ x: 4, y: 0 }),
-    new Vector({ x: 0, y: 11 }),
-    new Vector({ x: 7, y: 0 }),
-    new Vector({ x: 1, y: 1 }),
-    new Vector({ x: 3, y: 3 }),
-    new Vector({ x: -7, y: -3 }),
-    new Vector({ x: -13, y: 70 }),
-];
-
 describe('Vector class:', () => {
-    test('normal value', () => {
-        for (const vector of vectors) {
-            expect(vector.normal).toMatchObject({ x: -vector.y, y: vector.x });
-        }
+    describe('values', () => {
+        const vectors = [
+            new Vector({ x: 4, y: 0 }),
+            new Vector({ x: 0, y: 11 }),
+            new Vector({ x: 7, y: 0 }),
+            new Vector({ x: 1, y: 1 }),
+            new Vector({ x: 3, y: 3 }),
+            new Vector({ x: -7, y: -3 }),
+            new Vector({ x: -13, y: 70 }),
+        ];
+
+        test('normal', () => {
+            for (const vector of vectors) {
+                expect(vector.normal).toMatchObject({ x: -vector.y, y: vector.x });
+            }
+        });
+
+        test('magnitude', () => {
+            for (const vector of vectors) {
+                expect(vector.magnitude).toBeCloseTo(Math.sqrt(vector.x ** 2 + vector.y ** 2));
+            }
+        });
+
+        test('unit', () => {
+            for (const vector of vectors) {
+                expect(vector.unit.magnitude).toBeCloseTo(1);
+            }
+        });
     });
 
-    test('magnitude value', () => {
-        for (const vector of vectors) {
-            expect(vector.magnitude).toBeCloseTo(Math.sqrt(vector.x ** 2 + vector.y ** 2));
-        }
-    });
+    describe('methods', () => {
+        const vector1 = new Vector({ x: 4, y: 2 });
+        const vector2 = new Vector({ x: 2, y: 4 });
 
-    test('unit value', () => {
-        for (const vector of vectors) {
-            expect(vector.unit.magnitude).toBeCloseTo(1);
-        }
+        test('getDot static method', () => {
+            expect(Vector.getDot(vector1, vector2)).toBe(vector1.x * vector2.x + vector1.y * vector2.y);
+        });
+
+        test('add method', () => {
+            expect(vector1.add(vector2)).toMatchObject({ x: 6, y: 6 });
+        });
+
+        test('subtr method', () => {
+            expect(vector1.subtr(vector2)).toMatchObject({ x: 2, y: -2 });
+        });
+
+        test('mult method', () => {
+            expect(vector1.mult(2)).toMatchObject({ x: 8, y: 4 });
+        });
     });
 });
