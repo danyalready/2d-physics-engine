52aa6510ca8a1bc4da34771fe86d17405ae334d2
danyalready <doniyor.otamurodov@gmail.com>
2023-12-05 01:54:30 +0500

    test: Ball - repositionate

--- src/classes/Ball.spec.ts
+++ src/classes/Ball.spec.ts
@@ -1,6 +1,19 @@
 import Ball from './Ball';
+import Vector from './Vector';
 
 describe('Ball class:', () => {
+    test('repositionate public method', () => {
+        const ball = new Ball({ accelerationUnit: 1, coordinate: { x: 0, y: 0 }, radius: 2 });
+
+        ball.velocity = new Vector({ x: 1, y: 1 });
+        for (let i = 0; i <= 10; i++) {
+            ball.repositionate();
+        }
+
+        expect(ball.position.x).toBeGreaterThan(5);
+        expect(ball.position.y).toBeGreaterThan(5);
+    });
+
     test('isCollision static method', () => {
         const ball1 = new Ball({ accelerationUnit: 0, coordinate: { x: 5, y: 5 }, radius: 15 });
         const ball2 = new Ball({ accelerationUnit: 0, coordinate: { x: 10, y: 5 }, radius: 10 });
