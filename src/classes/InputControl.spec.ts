306405636eca6fc22baed6c446eef3282ba06c42
danyalready <doniyor.otamurodov@gmail.com>
2023-12-19 01:17:27 +0500

    test: fix input-contorl tests

--- src/classes/InputControl.spec.ts
+++ src/classes/InputControl.spec.ts
@@ -1,36 +1,48 @@
 import InputControl from './InputControl';
+import Circle from './Circle';
 
 describe('InputControl class:', () => {
-    test('event listener variables are falsy by default', () => {
-        const inputControl = new InputControl();
+    const circle = new Circle({
+        mass: 1,
+        friction: 0,
+        elasticity: 0,
+        coordinate: { x: 0, y: 0 },
+        radius: 1,
+        accelerationUnit: 1,
+        color: 'green',
+        isPlayer: true,
+    });
 
-        expect(inputControl.arrowUp).toBeFalsy();
-        expect(inputControl.arrowRight).toBeFalsy();
-        expect(inputControl.arrowDown).toBeFalsy();
-        expect(inputControl.arrowLeft).toBeFalsy();
+    test('event listener variables are falsy by default', () => {
+        const inputControl = new InputControl(circle);
+
+        expect(inputControl.keys.arrowUp).toBeFalsy();
+        expect(inputControl.keys.arrowRight).toBeFalsy();
+        expect(inputControl.keys.arrowDown).toBeFalsy();
+        expect(inputControl.keys.arrowLeft).toBeFalsy();
     });
 
     test('event listener variables should get updated on key presses', () => {
-        const inputControl = new InputControl();
+        const inputControl = new InputControl(circle);
 
         window.dispatchEvent(new KeyboardEvent('keydown', { code: 'ArrowUp' }));
         window.dispatchEvent(new KeyboardEvent('keydown', { code: 'ArrowRight' }));
         window.dispatchEvent(new KeyboardEvent('keydown', { code: 'ArrowDown' }));
         window.dispatchEvent(new KeyboardEvent('keydown', { code: 'ArrowLeft' }));
 
-        expect(inputControl.arrowUp).toBeTruthy();
-        expect(inputControl.arrowRight).toBeTruthy();
-        expect(inputControl.arrowDown).toBeTruthy();
-        expect(inputControl.arrowLeft).toBeTruthy();
+        expect(inputControl.keys.arrowUp).toBeTruthy();
+        expect(inputControl.keys.arrowRight).toBeTruthy();
+        expect(inputControl.keys.arrowDown).toBeTruthy();
+        expect(inputControl.keys.arrowLeft).toBeTruthy();
 
         window.dispatchEvent(new KeyboardEvent('keyup', { code: 'ArrowUp' }));
         window.dispatchEvent(new KeyboardEvent('keyup', { code: 'ArrowRight' }));
         window.dispatchEvent(new KeyboardEvent('keyup', { code: 'ArrowDown' }));
         window.dispatchEvent(new KeyboardEvent('keyup', { code: 'ArrowLeft' }));
 
-        expect(inputControl.arrowUp).toBeFalsy();
-        expect(inputControl.arrowRight).toBeFalsy();
-        expect(inputControl.arrowDown).toBeFalsy();
-        expect(inputControl.arrowLeft).toBeFalsy();
+        expect(inputControl.keys.arrowUp).toBeFalsy();
+        expect(inputControl.keys.arrowRight).toBeFalsy();
+        expect(inputControl.keys.arrowDown).toBeFalsy();
+        expect(inputControl.keys.arrowLeft).toBeFalsy();
     });
 });
