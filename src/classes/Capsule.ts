97fddb5186a0903f93db35e98c4a2edc87d33e79
danyalready <doniyor.otamurodov@gmail.com>
2024-01-09 19:36:38 +0500

    feat: capsule class initial properties

--- src/classes/Capsule.ts
+++ src/classes/Capsule.ts
@@ -1,10 +1,60 @@
 import PhysicalObject, { type PhysicalObjectParams } from './PhysicalObject';
+import Vector from './Vector';
 
-type CapsuleParams = PhysicalObjectParams & {};
+type CapsuleParams = PhysicalObjectParams & {
+    radius: number;
+    length: number;
+};
 
 class Capsule extends PhysicalObject {
+    public radius: number;
+    public positionEnd: Vector;
+    public refPositionStart: Vector;
+    public refPositionEnd: Vector;
+
     constructor(params: CapsuleParams) {
         super(params);
+
+        this.radius = params.radius;
+        this.positionEnd = new Vector({ x: this.positionStart.x + params.length, y: this.positionStart.y });
+        this.refPositionStart = this.positionStart;
+        this.refPositionEnd = this.positionEnd;
+    }
+
+    public get positionStart(): Vector {
+        return this.position;
+    }
+
+    public set positionStart(vector: Vector) {
+        this.position = vector;
+    }
+
+    public get positionCenter(): Vector {
+        return this.refPositionStart.add(this.refPositionEnd).mult(0.5);
+    }
+
+    public get refUnit(): Vector {
+        return this.refPositionEnd.subtr(this.refPositionStart).unit;
+    }
+
+    public get length(): number {
+        return this.refPositionStart.subtr(this.refPositionEnd).magnitude;
+    }
+
+    public draw(ctx: CanvasRenderingContext2D) {
+        // POSITION START ARC
+        ctx.beginPath();
+        ctx.arc(this.positionStart.x, this.positionStart.y, this.radius, this.angle, this.angle + Math.PI);
+        ctx.strokeStyle = 'black';
+        ctx.stroke();
+        ctx.closePath();
+
+        // POSITION END ARC
+        ctx.beginPath();
+        ctx.arc(this.positionEnd.x, this.positionEnd.y, this.radius, this.angle, this.angle + Math.PI, true);
+        ctx.strokeStyle = 'red';
+        ctx.stroke();
+        ctx.closePath();
     }
 }
 
