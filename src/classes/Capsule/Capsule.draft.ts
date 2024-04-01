2e62474f38060b0f27791c9d74afc12ebe4fe9c5
danyalready <doniyor.otamurodov@gmail.com>
2024-04-02 01:42:11 +0500

    refactor: implement Capsule with components

---
+++ src/classes/Capsule/Capsule.draft.ts
@@ -0,0 +1,90 @@
+import Circle, { type CircleParams } from '../Circle/Circle';
+import Vector from '../Vector/Vector';
+import Matrix from '../Matrix/Matrix';
+
+type CapsuleParams = CircleParams & {
+    length: number;
+};
+
+class Capsule extends Circle {
+    public length: number;
+    public positionStart: Vector;
+    public positionEnd: Vector;
+    public readonly refPositionStart: Vector;
+    public readonly refPositionEnd: Vector;
+
+    constructor(params: CapsuleParams) {
+        super(params);
+
+        this.radius = params.radius;
+        this.length = params.length;
+        this.positionStart = new Vector({ x: this.position.x - params.length / 2, y: this.position.y });
+        this.positionEnd = new Vector({ x: this.position.x + params.length / 2, y: this.position.y });
+
+        this.refPositionStart = new Vector({ ...this.positionStart });
+        this.refPositionEnd = new Vector({ ...this.positionEnd });
+    }
+
+    public get unit(): Vector {
+        return this.refPositionEnd.subtr(this.refPositionStart).unit;
+    }
+
+    public repositionate(): void {
+        super.repositionate();
+
+        const capsuleRotationMatrix = Matrix.getRotationMatrix(this.angle);
+        const capsuleUnitMatrix = new Matrix(2, 1);
+
+        capsuleUnitMatrix.data = [[this.unit.x], [this.unit.y]];
+
+        const capsuleRotatedUnitMatrix = capsuleRotationMatrix.mult(capsuleUnitMatrix);
+        const capsuleRotatedUnit = new Vector({
+            x: capsuleRotatedUnitMatrix.data[0][0],
+            y: capsuleRotatedUnitMatrix.data[1][0],
+        });
+
+        this.positionStart = this.position.add(capsuleRotatedUnit.mult(-this.length / 2));
+        this.positionEnd = this.position.add(capsuleRotatedUnit.mult(this.length / 2));
+    }
+
+    public getClosestPointTo(vector: Vector): Vector {
+        const vectorToPositionStart = this.positionStart.subtr(vector);
+        const positionEndToVector = vector.subtr(this.positionEnd);
+
+        if (Vector.getDot(this.unit, vectorToPositionStart) > 0) {
+            return this.positionStart;
+        }
+
+        if (Vector.getDot(this.unit, positionEndToVector) > 0) {
+            return this.positionEnd;
+        }
+
+        const scalar = Vector.getDot(this.unit, vectorToPositionStart);
+        const closestVector = this.unit.mult(scalar);
+
+        return this.positionStart.subtr(closestVector);
+    }
+
+    public draw(ctx: CanvasRenderingContext2D) {
+        ctx.beginPath();
+        ctx.arc(
+            this.positionStart.x,
+            this.positionStart.y,
+            this.radius,
+            this.angle + Math.PI / 2,
+            this.angle + Math.PI * 1.5,
+        );
+        ctx.arc(
+            this.positionEnd.x,
+            this.positionEnd.y,
+            this.radius,
+            this.angle + Math.PI * 1.5,
+            this.angle + Math.PI / 2,
+        );
+        ctx.fillStyle = 'red';
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
+export default Capsule;
--- src/classes/Capsule/Capsule.ts
+++ src/classes/Capsule/Capsule.ts
@@ -1,38 +1,51 @@
-import Circle, { type CircleParams } from '../Circle/Circle';
-import Vector from '../Vector/Vector';
-import Matrix from '../Matrix/Matrix';
+import { Circle, Matrix, Vector } from '..';
 
-type CapsuleParams = CircleParams & {
+type CapsuleParams = {
     length: number;
+    position: Vector;
+    radius?: number;
+    components?: [Circle, Circle];
 };
 
-class Capsule extends Circle {
+class Capsule {
+    /** The distance between the components of Capsule. */
     public length: number;
-    public positionStart: Vector;
-    public positionEnd: Vector;
+
+    /** The center point of Capsule. */
+    public position: Vector;
+
+    public components: [Circle, Circle];
+
+    /** The initial position of the first component of Capsule. */
     public readonly refPositionStart: Vector;
+
+    /** The initial position of the second component of Capsule. */
     public readonly refPositionEnd: Vector;
 
     constructor(params: CapsuleParams) {
-        super(params);
-
-        this.radius = params.radius;
         this.length = params.length;
-        this.positionStart = new Vector({ x: this.position.x - params.length / 2, y: this.position.y });
-        this.positionEnd = new Vector({ x: this.position.x + params.length / 2, y: this.position.y });
+        this.position = params.position;
+        this.components = params.components || [
+            new Circle({
+                coordinate: this.position.subtr(new Vector({ x: this.position.x - this.length / 2, y: 0 })),
+                radius: params.radius || 10,
+            }),
+            new Circle({
+                coordinate: this.position.subtr(new Vector({ x: this.position.x + this.length / 2, y: 0 })),
+                radius: params.radius || 10,
+            }),
+        ];
 
-        this.refPositionStart = new Vector({ ...this.positionStart });
-        this.refPositionEnd = new Vector({ ...this.positionEnd });
+        this.refPositionStart = this.components[0].position;
+        this.refPositionEnd = this.components[1].position;
     }
 
     public get unit(): Vector {
         return this.refPositionEnd.subtr(this.refPositionStart).unit;
     }
 
-    public repositionate(): void {
-        super.repositionate();
-
-        const capsuleRotationMatrix = Matrix.getRotationMatrix(this.angle);
+    public reposition(): void {
+        const capsuleRotationMatrix = Matrix.getRotationMatrix(0); // TODO: this.angle
         const capsuleUnitMatrix = new Matrix(2, 1);
 
         capsuleUnitMatrix.data = [[this.unit.x], [this.unit.y]];
@@ -43,43 +56,44 @@ class Capsule extends Circle {
             y: capsuleRotatedUnitMatrix.data[1][0],
         });
 
-        this.positionStart = this.position.add(capsuleRotatedUnit.mult(-this.length / 2));
-        this.positionEnd = this.position.add(capsuleRotatedUnit.mult(this.length / 2));
+        this.components[0].position = this.position.add(capsuleRotatedUnit.mult(-this.length / 2));
+        this.components[1].position = this.position.add(capsuleRotatedUnit.mult(this.length / 2));
     }
 
+    /** Returns the closest point of Capsule to the given vector. */
     public getClosestPointTo(vector: Vector): Vector {
-        const vectorToPositionStart = this.positionStart.subtr(vector);
-        const positionEndToVector = vector.subtr(this.positionEnd);
+        const vectorToPositionStart = this.components[0].position.subtr(vector);
+        const positionEndToVector = vector.subtr(this.components[1].position);
 
         if (Vector.getDot(this.unit, vectorToPositionStart) > 0) {
-            return this.positionStart;
+            return this.components[0].position;
         }
 
         if (Vector.getDot(this.unit, positionEndToVector) > 0) {
-            return this.positionEnd;
+            return this.components[1].position;
         }
 
         const scalar = Vector.getDot(this.unit, vectorToPositionStart);
         const closestVector = this.unit.mult(scalar);
 
-        return this.positionStart.subtr(closestVector);
+        return this.components[0].position.subtr(closestVector);
     }
 
     public draw(ctx: CanvasRenderingContext2D) {
         ctx.beginPath();
         ctx.arc(
-            this.positionStart.x,
-            this.positionStart.y,
-            this.radius,
-            this.angle + Math.PI / 2,
-            this.angle + Math.PI * 1.5,
+            this.components[0].position.x,
+            this.components[0].position.y,
+            this.components[0].radius,
+            0 + Math.PI / 2, // TODO: this.angle
+            0 + Math.PI * 1.5, // TODO: this.angle
         );
         ctx.arc(
-            this.positionEnd.x,
-            this.positionEnd.y,
-            this.radius,
-            this.angle + Math.PI * 1.5,
-            this.angle + Math.PI / 2,
+            this.components[1].position.x,
+            this.components[1].position.y,
+            0, // TODO: this.angle
+            0 + Math.PI * 1.5, // TODO: this.angle
+            0 + Math.PI / 2, // TODO: this.angle
         );
         ctx.fillStyle = 'red';
         ctx.fill();
