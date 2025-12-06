ed094b7543df6e3a1f32c0354f4998d1a05a49d5
Doniyor <doniyorgit@gmail.com>
2025-12-06 19:54:33 +0500

    fix: missing position in circle-collider

--- src/components/ColliderComponents/CircleCollider.component.ts
+++ src/components/ColliderComponents/CircleCollider.component.ts
@@ -1,5 +1,6 @@
 import { AABB } from '../../math/AABB';
 import { Vector2 } from '../../math/Vector2';
+import { Transform } from '../Transform.component';
 import { Collider } from './Collider.abstract';
 
 export class CircleCollider extends Collider {
@@ -14,8 +15,13 @@ export class CircleCollider extends Collider {
         return this.radius;
     }
 
-    getAABB(): AABB {
-        return new AABB(new Vector2(-this.radius, -this.radius), new Vector2(this.radius, this.radius));
+    getAABB(transform: Transform): AABB {
+        const position = transform.getPosition();
+
+        return new AABB(
+            new Vector2(position.x - this.radius, position.y - this.radius),
+            new Vector2(position.x + this.radius, position.y + this.radius),
+        );
     }
 
     calculateInertia(mass: number): number {
