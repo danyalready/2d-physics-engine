d0a0e8ab00bff759a79c8e44e550e6b2c9c087c8
Doniyor <doniyorgit@gmail.com>
2025-12-07 17:19:36 +0500

    feat: box-drawer options

--- src/components/DrawerComponents/BoxDrawer.component.ts
+++ src/components/DrawerComponents/BoxDrawer.component.ts
@@ -1,11 +1,14 @@
 import { Transform } from '../Transform.component';
 import { Drawer } from './Drawer.component';
 
+interface BoxDrawerProps {
+    size: { width: number; height: number };
+    strokeColor?: string;
+    fillColor?: string;
+}
+
 export class BoxDrawer extends Drawer {
-    constructor(
-        private width: number,
-        private height: number,
-    ) {
+    constructor(private options: BoxDrawerProps) {
         super();
     }
 
@@ -19,8 +22,27 @@ export class BoxDrawer extends Drawer {
         canvasCtx.translate(x, y);
         canvasCtx.rotate(rotation);
 
-        // Draw rectangle centered at origin
-        canvasCtx.strokeRect(-this.width / 2, -this.height / 2, this.width, this.height);
+        // Fill box if fillColor is provided
+        if (this.options.fillColor) {
+            canvasCtx.fillStyle = this.options.fillColor;
+            canvasCtx.fillRect(
+                -this.options.size.width / 2,
+                -this.options.size.height / 2,
+                this.options.size.width,
+                this.options.size.height,
+            );
+        }
+
+        // Stroke box
+        if (this.options.strokeColor) {
+            canvasCtx.strokeStyle = this.options.strokeColor;
+            canvasCtx.strokeRect(
+                -this.options.size.width / 2,
+                -this.options.size.height / 2,
+                this.options.size.width,
+                this.options.size.height,
+            );
+        }
 
         canvasCtx.restore();
     }
